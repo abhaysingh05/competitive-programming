e5efe265f02a6872c529aaf132acfb07cec1e79a
Abhay Singh <abhaysingh0948@gmail.com>
2024-03-24 14:09:20 +0530

    adding snippets

---
+++ snippets/main-snippet.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ff first
+#define ss second
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define int long long
+const int INF = (int)1e9;
+const int mod = INF + 7;
+/**************************************************************************************/
+
+void runCase(int &testCase) {
+    // cout << "#Case " << testCase << ": \n";
+
+    // Your code here
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int tests = 1;
+    cin >> tests;
+    for (int i = 1; i <= t; i++) runCase(i);
+
+    return 0;
+}
